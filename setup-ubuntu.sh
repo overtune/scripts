f8ddce38965ae620e1e7cc536837551914e3e1b0
Johan Runesson <info@johanrunesson.se>
2018-10-08 17:52:43 +0000

    Adding dotfiles

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,2 +1,12 @@
-sudo apt-get update &&
-sudo apt-get install vim tmux git mosh -y
+# Update and install software
+sudo apt-get update
+sudo apt-get install vim tmux git zsh mosh -y
+
+# Install Oh-my-shell
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Get the dotfiles
+cd ~
+git clone https://github.com/overtune/dotfiles.git
+cd dotfiles
+bash makesymlinks.sh
