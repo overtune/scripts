0d01ef34b55c93cff44bfe8c876ddb0a24b0a336
Johan Runesson <info@johanrunesson.se>
2021-03-30 11:22:29 +0200

    Updating installation script

--- setup-mac.sh
+++ setup-mac.sh
@@ -19,13 +19,16 @@ printf "${OK}Install Xcode and command line tools\n"
 xcode-select --install
 
 # Install Homebrew 
-printf "${OK}Install Homebrew\n"
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh 
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+	printf "${OK}Install Homebrew\n"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 # Install brews
 # printf "${OK}Install git\n"
 # brew install git
-printf "${OK}Install vim\n"
+printf "${OK}Install neovim\n"
 brew install vim
 printf "${OK}Install tmux\n"
 brew install tmux
@@ -41,24 +44,35 @@ printf "${OK}Install fff\n"
 brew install fff
 printf "${OK}Install fzf\n"
 brew install fzf
+printf "${OK}Install lazygit\n"
+brew install jesseduffield/lazygit/lazygit
+printf "${OK}Install lazydocker\n"
+brew install jesseduffield/lazydocker/lazydocker
+printf "${OK}Install reattach-to-user-namespace\n"
+brew install reattach-to-user-namespace
 $(brew --prefix)/opt/fzf/install
 printf "${OK}Install iterm2\n"
 brew install --cask iterm2
-printf "${OK}Install alacritty\n"
-brew install --cask alacritty
+# printf "${OK}Install alacritty\n"
+# brew install --cask alacritty
 printf "${OK}Install docker\n"
 brew install --cask docker
 printf "${OK}Install qmktoolbox\n"
 brew tap homebrew/cask-drivers
 brew install --cask qmk-toolbox
 
+# Setup neovim 
+printf "${OK}Install neovim dependencies\n"
+npm install -g neovim
+python3 -m pip install --user --upgrade pynvim
+
 # Install mas 
-printf "${OK}Install mas\n"
-brew install mas
-mas signin $email
-printf "${OK}Install iA Writer\n"
-mas install 775737590 # iA Writer
-printf "${OK}Install Lastpass\n"
-mas install 926036361 # Lastpass
-printf "${OK}Install Magnet\n"
-mas install 441258766 # Magnet
+# printf "${OK}Install mas\n"
+# brew install mas
+# mas signin $email
+# printf "${OK}Install iA Writer\n"
+# mas install 775737590 # iA Writer
+# printf "${OK}Install Lastpass\n"
+# mas install 926036361 # Lastpass
+# printf "${OK}Install Magnet\n"
+# mas install 441258766 # Magnet
