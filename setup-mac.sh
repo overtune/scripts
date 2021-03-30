0e4f5f6930d5572061403dbbf75eb67199ca6a09
Johan Runesson <info@johanrunesson.se>
2021-03-30 09:05:53 +0200

    WIP: Testing params

--- setup-mac.sh
+++ setup-mac.sh
@@ -13,53 +13,55 @@ do
     esac
 done
 
-# Setup folder structure
-printf "${OK}Setup folder structure\n"
-mkdir -p  ~/Development/work 
-mkdir -p  ~/Development/private
+echo $email
 
-# Install Xcode and command line tools
-printf "${OK}Install Xcode and command line tools\n"
-xcode-select --install
+# # Setup folder structure
+# printf "${OK}Setup folder structure\n"
+# mkdir -p  ~/Development/work 
+# mkdir -p  ~/Development/private
 
-# Install Homebrew 
-printf "${OK}Install Homebrew\n"
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh 
+# # Install Xcode and command line tools
+# printf "${OK}Install Xcode and command line tools\n"
+# xcode-select --install
 
-# Install brews
-# printf "${OK}Install git\n"
-# brew install git
-printf "${OK}Install vim\n"
-brew install vim
-printf "${OK}Install tmux\n"
-brew install tmux
-printf "${OK}Install nodejs\n"
-brew install nodejs
-printf "${OK}Install python3\n"
-brew install python3
-printf "${OK}Install alacritty\n"
-brew install alacritty
-printf "${OK}Install docker\n"
-brew install docker
-printf "${OK}Install qmktoolbox\n"
-brew install qmktoolbox
-printf "${OK}Install thefuck\n"
-brew install thefuck
-printf "${OK}Install ack\n"
-brew install ack
-printf "${OK}Install fff\n"
-brew install fff
-printf "${OK}Install fzf\n"
-brew install fzf
-$(brew --prefix)/opt/fzf/install
+# # Install Homebrew 
+# printf "${OK}Install Homebrew\n"
+# curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh 
 
-# Install mas 
-printf "${OK}Install mas\n"
-brew install mas
-mas signin $email
-printf "${OK}Install iA Writer\n"
-mas install 775737590 # iA Writer
-printf "${OK}Install Lastpass\n"
-mas install 926036361 # Lastpass
-printf "${OK}Install Magnet\n"
-mas install 441258766 # Magnet
+# # Install brews
+# # printf "${OK}Install git\n"
+# # brew install git
+# printf "${OK}Install vim\n"
+# brew install vim
+# printf "${OK}Install tmux\n"
+# brew install tmux
+# printf "${OK}Install nodejs\n"
+# brew install nodejs
+# printf "${OK}Install python3\n"
+# brew install python3
+# printf "${OK}Install alacritty\n"
+# brew install alacritty
+# printf "${OK}Install docker\n"
+# brew install docker
+# printf "${OK}Install qmktoolbox\n"
+# brew install qmktoolbox
+# printf "${OK}Install thefuck\n"
+# brew install thefuck
+# printf "${OK}Install ack\n"
+# brew install ack
+# printf "${OK}Install fff\n"
+# brew install fff
+# printf "${OK}Install fzf\n"
+# brew install fzf
+# $(brew --prefix)/opt/fzf/install
+
+# # Install mas 
+# printf "${OK}Install mas\n"
+# brew install mas
+# mas signin $email
+# printf "${OK}Install iA Writer\n"
+# mas install 775737590 # iA Writer
+# printf "${OK}Install Lastpass\n"
+# mas install 926036361 # Lastpass
+# printf "${OK}Install Magnet\n"
+# mas install 441258766 # Magnet
