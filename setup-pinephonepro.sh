817c75b03a1a596c22c0c121dd8694d297c1785c
Johan Runesson <info@johanrunesson.se>
2022-02-06 16:47:01 +0100

    Adding comments

--- setup-pinephonepro.sh
+++ setup-pinephonepro.sh
@@ -1,16 +1,26 @@
+#!/bin/bash
+
 # TODO: Only run commands once
+
+# Go home
 cd $HOME
 
-sudo pacman -S vim tmux git zsh
+# Install software
+sudo pacman -S --needed vim tmux git zsh
 
+# Install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
 
+# Setup dotfiles
 git clone --bare https://github.com/overtune/dotfiles.git $HOME/.dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 config checkout
 
+# Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.zshrc
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# Install node
 nvm install node
